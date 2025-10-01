0f94b31af314acc513ea1abacf15ccfebff9ffd9
sondong <sondong23092005@gmail.com>
2025-10-01 14:59:13 +0700

    feat: base project

---
+++ src/main/java/com/backend/vinbook/VinbookApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vinbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/vinbook/VinbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vinbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
